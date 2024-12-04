72e6f1d81732abf93c5f53b8c878b5a539183087
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2024-12-04 13:43:23 +0000

    Updated: config/, scripts/ with bcosdensenet121

---
+++ scripts/image/bcosdensenet121.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --partition=gpu_min32gb
+#SBATCH --qos=gpu_min32gb
+#SBATCH --job-name=cind_breloai_bcos_ret
+#SBATCH --output=ConViT_Tiny.out
+#SBATCH --error=ConViT_Tiny.err
+
+
+
+echo "CINDERELLA BreLoAI Retrieval: A Study with Attention Mechanisms"
+echo "Training Catalogue Type: E"
+python src/main_image.py \
+ --gpu_id 0 \
+ --config_json 'config/image/E/bcosdensenet121.json' \
+ --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/E' \
+ --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E' \
+ --train_or_test 'train'
+# echo "Testing Catalogue Type: E"
+# python src/main_image.py \
+#  --gpu_id 0 \
+#  --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/E' \
+#  --train_or_test 'test' \
+#  --checkpoint_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E/2024-11-16_22-16-52/' \
+#  --verbose
+# echo "Finished"
+
+# echo "Training Catalogue Type: F"
+# python src/main_image.py \
+ --gpu_id 0 \
+ --config_json 'config/image/F/bcosdensenet121.json' \
+ --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/F' \
+ --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F' \
+ --train_or_test 'train'
+echo "Testing Catalogue Type: F"
+# python src/main_image.py \
+#  --gpu_id 0 \
+#  --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/F' \
+#  --train_or_test 'test' \
+#  --checkpoint_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F/2024-11-17_01-29-30/' \
+#  --verbose
+echo "Finished"
