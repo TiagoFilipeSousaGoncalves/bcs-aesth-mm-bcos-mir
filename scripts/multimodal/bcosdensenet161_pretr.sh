fe5b5c4edc689f4d4a3db65ad81fa2eb777ff57f
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2024-12-22 23:28:22 +0000

    Updated: config/, scripts/

---
+++ scripts/multimodal/bcosdensenet161_pretr.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#SBATCH --partition=gpu_min12gb
+#SBATCH --qos=gpu_min12gb
+#SBATCH --job-name=cind_breloai_att_ret
+#SBATCH --output=bcosdensenet161_pretr.out
+#SBATCH --error=bcosdensenet161_pretr.err
+
+
+
+echo "CINDERELLA BreLoAI Retrieval: A Study with Multi-modal B-cos Networks"
+echo "Training Catalogue Type: E"
+python src/main_multimodal.py \
+ --gpu_id 0 \
+ --config_json 'config/multimodal/E/bcosdensenet161_pretr.json' \
+ --csvs_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db/csvs' \
+ --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/E' \
+ --img_model_weights_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E/2024-12-18_02-13-49/bin/model_final.pt' \
+ --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E'
+echo "Finished"
+# echo "Testing Catalogue Type: E"
+# python src/main_multimodal.py \
+#  --gpu_id 0 \
+#  --csvs_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db/csvs' \
+#  --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/E' \
+#  --img_model_weights_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E/2024-12-18_02-13-49/bin/model_final.pt' \
+#  --checkpoint_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E/TBA/' \
+#  --train_or_test 'test' \
+#  --verbose
+# echo "Finished"
+
+echo "Training Catalogue Type: F"
+python src/main_multimodal.py \
+ --gpu_id 0 \
+ --config_json 'config/multimodal/F/bcosdensenet161_pretr.json' \
+ --csvs_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db/csvs' \
+ --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/F' \
+ --img_model_weights_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F/2024-12-19_05-08-40/bin/model_final.pt' \
+ --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F'
+echo "Finished"
+# echo "Testing Catalogue Type: F"
+# python src/main_multimodal.py \
+#  --gpu_id 0 \
+#  --csvs_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db/csvs' \
+#  --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/F' \
+#  --img_model_weights_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F/2024-12-19_05-08-40/bin/model_final.pt' \
+#  --checkpoint_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F/TBA/' \
+#  --train_or_test 'test' \
+#  --verbose
+# echo "Finished"
