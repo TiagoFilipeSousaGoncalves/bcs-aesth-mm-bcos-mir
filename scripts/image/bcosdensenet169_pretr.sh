c3d46bf7a447056ad925618678823ff22201af79
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2024-12-22 23:45:24 +0000

    Updated: config/, scripts/

--- scripts/image/bcosdensenet169_pretr.sh
+++ scripts/image/bcosdensenet169_pretr.sh
@@ -7,38 +7,38 @@
 
 
 
-echo "CINDERELLA BreLoAI Retrieval: A Study with Bcos Networks"
-echo "Training Catalogue Type: E"
-python src/main_image.py \
- --gpu_id 0 \
- --config_json 'config/image/E/bcosdensenet169_pretr.json' \
- --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/E' \
- --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E' \
- --train_or_test 'train'
-echo "Finished"
-# echo "Testing Catalogue Type: E"
+echo "CINDERELLA BreLoAI Retrieval: A Study with B-cos Networks"
+# echo "Training Catalogue Type: E"
 # python src/main_image.py \
 #  --gpu_id 0 \
-#  --config_json 'config/image/E/DaViT_Tiny.json' \
+#  --config_json 'config/image/E/bcosdensenet169_pretr.json' \
 #  --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/E' \
-#  --train_or_test 'test' \
-#  --checkpoint_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E/2024-11-16_22-23-51/' \
-#  --verbose
+#  --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E' \
+#  --train_or_test 'train'
 # echo "Finished"
+echo "Testing Catalogue Type: E"
+python src/main_image.py \
+ --gpu_id 0 \
+ --config_json 'config/image/E/DaViT_Tiny.json' \
+ --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/E' \
+ --train_or_test 'test' \
+ --checkpoint_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E/2024-12-18_20-55-34/' \
+ --verbose
+echo "Finished"
 
 echo "Training Catalogue Type: F"
-python src/main_image.py \
- --gpu_id 0 \
- --config_json 'config/image/F/bcosdensenet169_pretr.json' \
- --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/F' \
- --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F' \
- --train_or_test 'train'
-echo "Finished"
-# echo "Testing Catalogue Type: F"
 # python src/main_image.py \
 #  --gpu_id 0 \
+#  --config_json 'config/image/F/bcosdensenet169_pretr.json' \
 #  --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/F' \
-#  --train_or_test 'test' \
-#  --checkpoint_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F/2024-11-17_02-20-44/' \
-#  --verbose
+#  --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F' \
+#  --train_or_test 'train'
 # echo "Finished"
+echo "Testing Catalogue Type: F"
+python src/main_image.py \
+ --gpu_id 0 \
+ --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/F' \
+ --train_or_test 'test' \
+ --checkpoint_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F/2024-12-20_16-02-46/' \
+ --verbose
+echo "Finished"
