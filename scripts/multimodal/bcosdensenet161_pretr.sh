732be6b7359f244aad79cbc39803802eb00205c1
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2024-12-23 01:04:17 +0000

    Updated: config/, scripts/

--- scripts/multimodal/bcosdensenet161_pretr.sh
+++ scripts/multimodal/bcosdensenet161_pretr.sh
@@ -8,42 +8,42 @@
 
 
 echo "CINDERELLA BreLoAI Retrieval: A Study with Multi-modal B-cos Networks"
-echo "Training Catalogue Type: E"
-python src/main_multimodal.py \
- --gpu_id 0 \
- --config_json 'config/multimodal/E/bcosdensenet161_pretr.json' \
- --csvs_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db/csvs' \
- --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/E' \
- --img_model_weights_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E/2024-12-18_02-13-49/bin/model_final.pt' \
- --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E'
-echo "Finished"
-# echo "Testing Catalogue Type: E"
+# echo "Training Catalogue Type: E"
 # python src/main_multimodal.py \
 #  --gpu_id 0 \
+#  --config_json 'config/multimodal/E/bcosdensenet161_pretr.json' \
 #  --csvs_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db/csvs' \
 #  --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/E' \
 #  --img_model_weights_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E/2024-12-18_02-13-49/bin/model_final.pt' \
-#  --checkpoint_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E/TBA/' \
-#  --train_or_test 'test' \
-#  --verbose
+#  --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E'
 # echo "Finished"
-
-echo "Training Catalogue Type: F"
+echo "Testing Catalogue Type: E"
 python src/main_multimodal.py \
  --gpu_id 0 \
- --config_json 'config/multimodal/F/bcosdensenet161_pretr.json' \
  --csvs_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db/csvs' \
- --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/F' \
- --img_model_weights_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F/2024-12-19_05-08-40/bin/model_final.pt' \
- --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F'
+ --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/E' \
+ --img_model_weights_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E/2024-12-18_02-13-49/bin/model_final.pt' \
+ --checkpoint_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/E/2024-12-22_23-29-20/' \
+ --train_or_test 'test' \
+ --verbose
 echo "Finished"
-# echo "Testing Catalogue Type: F"
+
+# echo "Training Catalogue Type: F"
 # python src/main_multimodal.py \
 #  --gpu_id 0 \
+#  --config_json 'config/multimodal/F/bcosdensenet161_pretr.json' \
 #  --csvs_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db/csvs' \
 #  --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/F' \
 #  --img_model_weights_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F/2024-12-19_05-08-40/bin/model_final.pt' \
-#  --checkpoint_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F/TBA/' \
-#  --train_or_test 'test' \
-#  --verbose
+#  --results_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F'
 # echo "Finished"
+echo "Testing Catalogue Type: F"
+python src/main_multimodal.py \
+ --gpu_id 0 \
+ --csvs_path '/nas-ctm01/datasets/private/CINDERELLA/breloai-web-db/csvs' \
+ --pickles_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/pickles/F' \
+ --img_model_weights_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F/2024-12-19_05-08-40/bin/model_final.pt' \
+ --checkpoint_path '/nas-ctm01/datasets/private/CINDERELLA/experiments/retrieval/tgoncalv/results/F/2024-12-22_23-39-03/' \
+ --train_or_test 'test' \
+ --verbose
+echo "Finished"
