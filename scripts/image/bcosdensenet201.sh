8c16b4139feaa578c98fc978782dfd0bd15b9bbc
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2024-12-26 10:32:55 +0000

    Updated: scripts/image

--- scripts/image/bcosdensenet201.sh
+++ scripts/image/bcosdensenet201.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --partition=gpu_min24gb
-#SBATCH --qos=gpu_min24gb
+#SBATCH --partition=gpu_min12gb
+#SBATCH --qos=gpu_min12gb
 #SBATCH --job-name=cind_breloai_bc_ret
 #SBATCH --output=bcosdensenet201.out
 #SBATCH --error=bcosdensenet201.err
